b8e7b53ff3a5161a805826bfe79092aafe96a99a
Oleksii Ishchenko <oleksiy26032005@gmail.com>
2026-01-12 23:56:20 +0200

    Add/adjust FluentValidation validators for auth

---
+++ backend/src/UrlShortener.Api/Validators/Auth/LoginRequestValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using UrlShortener.Api.Contracts.Auth;
+
+namespace UrlShortener.Api.Validators.Auth;
+
+public sealed class LoginRequestValidator : AbstractValidator<LoginRequest>
+{
+    public LoginRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(256);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MaximumLength(128);
+    }
+}
---
+++ backend/src/UrlShortener.Api/Validators/Auth/RegisterRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using UrlShortener.Api.Contracts.Auth;
+
+namespace UrlShortener.Api.Validators.Auth;
+
+public sealed class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+    public RegisterRequestValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .EmailAddress()
+            .MaximumLength(256);
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8)
+            .MaximumLength(128)
+            .Matches(@"\d").WithMessage("Password must contain at least one digit.");
+    }
+}
