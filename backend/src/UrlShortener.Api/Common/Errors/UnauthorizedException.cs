f00210147a07771e1a8784b14292b9fdfb9f987f
Oleksii Ishchenko <oleksiy26032005@gmail.com>
2026-01-16 22:53:39 +0200

    feat(errors): add UnauthorizedException

--- backend/src/UrlShortener.Api/Common/Errors/UnauthorizedException.cs
+++ backend/src/UrlShortener.Api/Common/Errors/UnauthorizedException.cs
@@ -1,6 +1,6 @@
 ﻿namespace UrlShortener.Api.Common.Errors;
 
-public class UnauthorizedException
+public class UnauthorizedException : ApiException
 {
-    
+    public UnauthorizedException(string message) : base(ApiErrorCodes.Unauthorized, message) { }
 }
