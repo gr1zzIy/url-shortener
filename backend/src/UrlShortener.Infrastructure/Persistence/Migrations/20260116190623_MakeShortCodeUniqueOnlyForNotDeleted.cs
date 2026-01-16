3c04024954d2df298c5751fa14f970a1db60c64e
Oleksii Ishchenko <oleksiy26032005@gmail.com>
2026-01-16 22:53:53 +0200

    feat(db): add refresh tokens migration

---
+++ backend/src/UrlShortener.Infrastructure/Persistence/Migrations/20260116190623_MakeShortCodeUniqueOnlyForNotDeleted.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortener.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeShortCodeUniqueOnlyForNotDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShortUrls_ShortCode",
+                table: "ShortUrls");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_ShortCode",
+                table: "ShortUrls",
+                column: "ShortCode",
+                unique: true,
+                filter: "\"DeletedAt\" IS NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ShortUrls_ShortCode",
+                table: "ShortUrls");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShortUrls_ShortCode",
+                table: "ShortUrls",
+                column: "ShortCode",
+                unique: true);
+        }
+    }
+}
